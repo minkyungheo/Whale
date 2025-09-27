b44e856f473522cc8253277f96e03a0861944195
허민경 <gjalsrud1234@gmail.com>
2025-09-27 14:24:49 +0900

    hello

---
+++ src/main/java/com/example/demo/controller/hello.java
@@ -0,0 +1,19 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+
+@RestController
+public class hello {
+
+    @GetMapping("/hello")
+    public String getMethodName() {
+        return "HELLO";
+    }
+    
+}
+
+
